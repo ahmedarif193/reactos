1dbf6a11649ec29426a8249b9f95dcbda5f6ec82
Ahmed ARIF <arif.ing@outlook.com>
2025-10-17 22:13:48 +0200

    [sdk] feat: port pseh amd64 frame handlers to c

---
+++ sdk/lib/pseh/amd64/framebased.c
@@ -0,0 +1,87 @@
+/*
+ * PROJECT:         ReactOS SEH Library
+ * LICENSE:         MIT (https://spdx.org/licenses/MIT)
+ * FILE:            sdk/lib/pseh/amd64/framebased.c
+ * PURPOSE:         Frame-based SEH support for AMD64
+ */
+
+#include <windef.h>
+#include <winnt.h>
+#include <excpt.h>
+#include <pseh/framebased/internal.h>
+
+#if defined(_MSC_VER)
+#include <intrin.h>
+#pragma intrinsic(__readeflags)
+#pragma intrinsic(__writeeflags)
+#pragma intrinsic(_ReturnAddress)
+#define PSEH_RETURN_ADDRESS() _ReturnAddress()
+#define PSEH_TLS __declspec(thread)
+#else
+#define PSEH_RETURN_ADDRESS() __builtin_return_address(0)
+#define PSEH_TLS __thread
+#endif
+
+NTSYSAPI
+VOID
+NTAPI
+RtlUnwind(
+    _In_opt_ PVOID TargetFrame,
+    _In_opt_ PVOID TargetIp,
+    _In_opt_ PEXCEPTION_RECORD ExceptionRecord,
+    _In_opt_ PVOID ReturnValue);
+
+static PSEH_TLS _SEHRegistration_t *PsehRegistrationTop;
+
+VOID
+__cdecl
+_SEHCleanHandlerEnvironment(VOID)
+{
+#if defined(_MSC_VER)
+    unsigned __int64 Eflags = __readeflags();
+    if (Eflags & 0x400ULL)
+    {
+        __writeeflags(Eflags & ~0x400ULL);
+    }
+#else
+    __asm__ __volatile__("cld" ::: "cc");
+#endif
+}
+
+_SEHRegistration_t *
+__cdecl
+_SEHCurrentRegistration(VOID)
+{
+    return PsehRegistrationTop;
+}
+
+_SEHRegistration_t *
+__cdecl
+_SEHRegisterFrame(
+    _In_ _SEHRegistration_t *Registration)
+{
+    Registration->SER_Prev = PsehRegistrationTop;
+    PsehRegistrationTop = Registration;
+    return Registration;
+}
+
+VOID
+__cdecl
+_SEHUnregisterFrame(VOID)
+{
+    if (PsehRegistrationTop != NULL)
+    {
+        PsehRegistrationTop = PsehRegistrationTop->SER_Prev;
+    }
+}
+
+VOID
+__cdecl
+_SEHGlobalUnwind(
+    _In_opt_ _SEHPortableFrame_t *TargetFrame)
+{
+    RtlUnwind(TargetFrame,
+              PSEH_RETURN_ADDRESS(),
+              NULL,
+              NULL);
+}
