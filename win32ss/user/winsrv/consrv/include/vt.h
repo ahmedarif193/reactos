24e4978391b3bbfdeae00c3cf24c725306d6279a
Ahmed ARIF <arif.ing@outlook.com>
2025-10-31 11:47:10 +0100

    [dll] fix build issue that caused by missing vt header

---
+++ win32ss/user/winsrv/consrv/include/vt.h
@@ -0,0 +1,24 @@
+/*
+ * COPYRIGHT:       See COPYING in the top level directory
+ * PROJECT:         ReactOS Console Server DLL
+ * FILE:            win32ss/user/winsrv/consrv/include/vt.h
+ * PURPOSE:         Virtual Terminal processing stubs
+ */
+
+#pragma once
+
+#include "conio.h"
+
+VOID
+NTAPI
+ConDrvVtInitializeBuffer(PTEXTMODE_SCREEN_BUFFER ScreenBuffer);
+
+NTSTATUS
+NTAPI
+ConDrvVtWriteConsole(PCONSOLE Console,
+                     PTEXTMODE_SCREEN_BUFFER ScreenBuffer,
+                     PCWSTR Buffer,
+                     ULONG Length,
+                     PULONG NumCharsProcessed,
+                     PBOOLEAN Handled);
+
