9e78f6982e83d81b56e2bebb307c5fa28e0bb8ec
Ahmed ARIF <arif.ing@outlook.com>
2025-10-31 17:54:49 +0100

    [apitests] Build winsta tests against NDK headers

--- modules/rostests/apitests/winsta/precomp.h
+++ modules/rostests/apitests/winsta/precomp.h
@@ -6,8 +6,7 @@
 #define COM_NO_WINDOWS_H
 
 #include <apitest.h>
-#include <winternl.h>
-#include <securitybaseapi.h>
+#include <ndk/ntndk.h>
 #include <ntstatus.h>
 
 #endif /* _WINSTA_APITEST_PRECOMP_H_ */
