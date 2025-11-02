c13be63f798101be28542e90f254a3915f4ce58a
Ahmed ARIF <arif.ing@outlook.com>
2025-11-02 17:27:26 +0100

    [PNP/Arbiter] arbport: Use PULONG for length/alignment outputs

--- ntoskrnl/io/pnpmgr/arb/arbport.c
+++ ntoskrnl/io/pnpmgr/arb/arbport.c
@@ -23,8 +23,8 @@ IopPortMemUnpackRequirements(
     _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor,
     _Out_ PUINT64 OutMinimumAddress,
     _Out_ PUINT64 OutMaximumAddress,
-    _Out_ PUINT32 OutLength,
-    _Out_ PUINT32 OutAlignment)
+    _Out_ PULONG OutLength,
+    _Out_ PULONG OutAlignment)
 {
     PAGED_CODE();
     DPRINT("IopPortMemUnpackRequirements: IoDescriptor: %p, OutMinimumAddress: %p, OutMaximumAddress: %p, OutLength: %p, OutAlignment: %p\n",
@@ -60,7 +60,7 @@ NTAPI
 IopPortMemUnpackResource(
     _In_ PCM_PARTIAL_RESOURCE_DESCRIPTOR CmDescriptor,
     _Out_ PUINT64 Start,
-    _Out_ PUINT32 OutLength)
+    _Out_ PULONG OutLength)
 {
     PAGED_CODE();
     DPRINT("IopPortMemUnpackResource: CmDescriptor: %p, Start: %p, OutLength: %p\n",
