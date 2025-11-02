2c643e570710239269f88a9393f6675b08096402
Ahmed ARIF <arif.ing@outlook.com>
2025-11-02 17:27:26 +0100

    [PNP/Arbiter] arbirq: Use PULONG for length/alignment outputs

--- ntoskrnl/io/pnpmgr/arb/arbirq.c
+++ ntoskrnl/io/pnpmgr/arb/arbirq.c
@@ -23,8 +23,8 @@ IopArbIrqUnpackRequirements(
     _In_ PIO_RESOURCE_DESCRIPTOR IoDescriptor,
     _Out_ PUINT64 OutMinimumAddress,
     _Out_ PUINT64 OutMaximumAddress,
-    _Out_ PUINT32 OutLength,
-    _Out_ PUINT32 OutAlignment)
+    _Out_ PULONG OutLength,
+    _Out_ PULONG OutAlignment)
 {
     PAGED_CODE();
     DPRINT("IopArbIrqUnpackRequirements: IoDescriptor: %p, OutMinimumAddress: %p, OutMaximumAddress: %p, OutLength: %p, OutAlignment: %p\n",
@@ -60,7 +60,7 @@ NTAPI
 IopArbIrqUnpackResource(
     _In_ PCM_PARTIAL_RESOURCE_DESCRIPTOR CmDescriptor,
     _Out_ PUINT64 Start,
-    _Out_ PUINT32 OutLength)
+    _Out_ PULONG OutLength)
 {
     PAGED_CODE();
     DPRINT("IopArbIrqUnpackResource: CmDescriptor: %p, Start: %p, OutLength: %p\n",
