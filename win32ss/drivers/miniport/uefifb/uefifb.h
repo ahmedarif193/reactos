99507d72f5b1c96e382fc26631b19a4fbefab676
Ahmed ARIF <arif.ing@outlook.com>
2025-11-02 13:28:24 +0100

    [uefifb] Add miniport internal headers

---
+++ win32ss/drivers/miniport/uefifb/uefifb.h
@@ -0,0 +1,73 @@
+/*
+ * PROJECT:         ReactOS UEFI Framebuffer miniport
+ * LICENSE:         BSD-3-Clause (https://spdx.org/licenses/BSD-3-Clause.html)
+ * PURPOSE:         UEFI GOP-backed framebuffer miniport driver
+ */
+
+#pragma once
+
+#include <ntdef.h>
+#include <dderror.h>
+#include <miniport.h>
+#include <video.h>
+#include <ntddvdeo.h>
+#include <devioctl.h>
+#include <ntstatus.h>
+
+#ifndef _LOADER_PARAMETER_FRAMEBUFFER_DEFINED
+#define _LOADER_PARAMETER_FRAMEBUFFER_DEFINED
+typedef struct _LOADER_PARAMETER_FRAMEBUFFER
+{
+    LARGE_INTEGER FrameBufferBase;
+    ULONG FrameBufferSize;
+    ULONG HorizontalResolution;
+    ULONG VerticalResolution;
+    ULONG PixelsPerScanLine;
+    ULONG PixelFormat;
+    ULONG RedMask;
+    ULONG GreenMask;
+    ULONG BlueMask;
+    ULONG Reserved;
+} LOADER_PARAMETER_FRAMEBUFFER, *PLOADER_PARAMETER_FRAMEBUFFER;
+#endif
+
+#ifndef EFI_GRAPHICS_PIXEL_FORMAT_DEFINED
+#define EFI_GRAPHICS_PIXEL_FORMAT_DEFINED
+#define PixelRedGreenBlueReserved8BitPerColor 0
+#define PixelBlueGreenRedReserved8BitPerColor 1
+#define PixelBitMask                           2
+#endif
+
+typedef struct _UEFIFB_DEVICE_EXTENSION
+{
+    LOADER_PARAMETER_FRAMEBUFFER FrameBufferInfo;
+    VIDEO_MODE_INFORMATION ModeInfo;
+    ULONG ModeCount;
+    PVIDEO_MODE_INFORMATION ModeTable; /* allocated list of modes; currently 1 (current GOP) */
+    ULONG CurrentModeIndex;
+    PVOID MappedFrameBuffer;
+    ULONG MappedLength;
+    BOOLEAN ModeSet;
+} UEFIFB_DEVICE_EXTENSION, *PUEFIFB_DEVICE_EXTENSION;
+
+BOOLEAN
+NTAPI
+InbvGetGopFrameBufferInfo(
+    _Out_ PLOADER_PARAMETER_FRAMEBUFFER FrameBufferInfo);
+
+/* Future GOP enumeration/switching (kernel stubs present) */
+BOOLEAN
+NTAPI
+InbvQueryGopModeCount(
+    _Out_ PULONG Count);
+
+BOOLEAN
+NTAPI
+InbvQueryGopModeInfo(
+    _In_ ULONG Index,
+    _Out_ PLOADER_PARAMETER_FRAMEBUFFER FrameBufferInfo);
+
+BOOLEAN
+NTAPI
+InbvSetGopMode(
+    _In_ ULONG Index);
