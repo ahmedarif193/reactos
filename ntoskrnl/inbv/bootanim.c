964708deeb2a6b211b59d6eb9403a17c44efcebc
Ahmed ARIF <arif.ing@outlook.com>
2026-01-24 01:29:58 +0100

    arm64: remove the fill black

--- ntoskrnl/inbv/bootanim.c
+++ ntoskrnl/inbv/bootanim.c
@@ -839,13 +839,8 @@ VOID
 NTAPI
 FinalizeBootLogo(VOID)
 {
-    /* Acquire lock and check the display state */
+    /* Acquire lock */
     InbvAcquireLock();
-    if (InbvGetDisplayState() == INBV_DISPLAY_STATE_OWNED)
-    {
-        /* Clear the screen */
-        VidSolidColorFill(0, 0, SCREEN_WIDTH-1, SCREEN_HEIGHT-1, BV_COLOR_BLACK);
-    }
 
     /* Reset progress bar and lock */
 #ifdef INBV_ROTBAR_IMPLEMENTED
