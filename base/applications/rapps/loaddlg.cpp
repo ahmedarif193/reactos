62376c7c22004ab45c0c3827b571bf0b7458db97
Ahmed ARIF <arif.ing@outlook.com>
2026-04-26 13:31:26 +0200

    [CLANG] Fix assorted compiler warnings.

--- base/applications/rapps/loaddlg.cpp
+++ base/applications/rapps/loaddlg.cpp
@@ -406,7 +406,7 @@ CertGetSubjectAndIssuer(HINTERNET hFile, CLocalPtr<char> &subjectInfo, CLocalPtr
         return FALSE;
     }
 
-    if (!flags & SECURITY_FLAG_SECURE)
+    if (!(flags & SECURITY_FLAG_SECURE))
     {
         return FALSE;
     }
