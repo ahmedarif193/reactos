11e23e6f122b286dcf9ca6a27de38964111faedb
Ahmed ARIF <arif.ing@outlook.com>
2026-05-09 19:25:15 +0200

    wip 1

--- boot/bootdata/livecd_start.cmd
+++ boot/bootdata/livecd_start.cmd
@@ -1,1 +1,15 @@
-@start %1
+@echo off
+
+if not "%1" == "" (
+    start %1
+    goto :eof
+)
+
+if not exist "%SystemRoot%\bin\kmtest_.exe" goto :eof
+
+pushd "%SystemRoot%\bin" || goto :eof
+
+echo Running kmtest_ MmSelfMap
+kmtest_.exe MmSelfMap
+
+popd
