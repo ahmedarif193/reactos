0af03120fc37a7d5fbebe3ad01681b8654405ed2
Ahmed ARIF <arif.ing@outlook.com>
2026-06-03 23:46:27 +0200

    [RPI5][VIDEOPRT] Add framebuffer miniport
    for now the miniport do it job, still lot of features but fo now it can let the desktop landing survive

--- win32ss/drivers/miniport/rpi5vc4/rpi5vc4_crtc.h
+++ win32ss/drivers/miniport/rpi5vc4/rpi5vc4_crtc.h
@@ -4,8 +4,8 @@
  * PURPOSE:     BCM2712 CRTC / PixelValve (timing generator) access.
  * COPYRIGHT:   Copyright 2026 Ahmed Arif <arif193@gmail.com>
  *
- * Ported from the Linux drm/vc4 driver (Dave Stevenson, Dom Cobley, Maxime
- * Ripard, Tim Gover) and adapted for ReactOS.
+ * Inspired by the only available driver out there, which is the Linux
+ * equivalent (drm/vc4).
  *
  * The PixelValve (PV) is the per-output timing generator that drives an HDMI
  * controller from an HVS output FIFO: it produces the HSYNC/VSYNC and active /
