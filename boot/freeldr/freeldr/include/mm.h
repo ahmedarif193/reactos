7c0378126c062f71447555b78fdec83e5bdddf79
Timo Kreuzer <timo.kreuzer@reactos.org>
2026-06-10 19:19:24 +0300

    [FREELDR] Remove MM_MAX_PAGE hack
    Allow 36 bits for the PFN (i.e. 256 TB)

--- boot/freeldr/freeldr/include/mm.h
+++ boot/freeldr/freeldr/include/mm.h
@@ -66,11 +66,7 @@ typedef struct _FREELDR_MEMORY_DESCRIPTOR
 #define MM_PAGE_SIZE    4096
 #define MM_PAGE_MASK    0xFFF
 #define MM_PAGE_SHIFT    12
-// CORE-20265: Expanded from 0x1FFFFF (8GB) to 0xFFFFFF (64GB).
-// The original 36-bit value (0xFFFFFFFFF = 262TB) produces impractically
-// large page lookup tables. 64GB is sufficient for typical VMs/hardware.
-//#define MM_MAX_PAGE        0xFFFFFFFFF /* 36 bits for the PFN */
-#define MM_MAX_PAGE        0xFFFFFF
+#define MM_MAX_PAGE        0xFFFFFFFFF /* 36 bits for the PFN */
 #define MM_MAX_PAGE_LOADER 0xFFFFF /* on x64 freeldr maps 4 GB */
 
 #define MM_SIZE_TO_PAGES(a)  \
