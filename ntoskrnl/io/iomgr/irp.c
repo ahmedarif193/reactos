9244f5e6d40ccd54cc7ca73bfc8c2101c3a148f9
Ahmed ARIF <arif.ing@outlook.com>
2026-06-11 00:54:29 +0200

    [NTOS][ARM64] Track DAIF across context switches and drain pended software interrupts on IRQL lowering

--- ntoskrnl/io/iomgr/irp.c
+++ ntoskrnl/io/iomgr/irp.c
@@ -359,6 +359,7 @@ IopCompleteRequest(IN PKAPC Apc,
         /* Check if we have an event or a file object */
         if (Irp->UserEvent)
         {
+            DbgPrint("IRPD irp=%p ev=%p type=%02x flags=%lx st=%lx thr=%p\n", Irp, Irp->UserEvent, Irp->UserEvent->Header.Type, Irp->Flags, Irp->IoStatus.Status, PsGetCurrentThread());
             /* At the very least, this is a PKEVENT, so signal it always */
             KeSetEvent(Irp->UserEvent, 0, FALSE);
 
@@ -1560,6 +1561,7 @@ IofCompleteRequest(IN PIRP Irp,
     /* Make sure the IRP isn't canceled */
     if (!Irp->Cancel)
     {
+        if (Irp->UserEvent) DbgPrint("IRPQ irp=%p ev=%p type=%02x iosb=%p thr=%p st=%lx irql=%u dpcact=%u cur=%p sad=%d\n", Irp, Irp->UserEvent, Irp->UserEvent->Header.Type, Irp->UserIosb, Thread, Irp->IoStatus.Status, KeGetCurrentIrql(), KeGetCurrentPrcb()->DpcRoutineActive, PsGetCurrentThread(), Thread->Tcb.SpecialApcDisable);
         /* Initialize the APC */
         KeInitializeApc(&Irp->Tail.Apc,
                         &Thread->Tcb,
