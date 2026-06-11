09eb0c84ca6c7f1360bbdbd09340c9c5dc1f03ff
Ahmed ARIF <arif.ing@outlook.com>
2026-06-11 22:38:01 +0200

    [FASTFAT] Make the Win8+ paths build with PSEH and the WDK gates
    FatCommonCreateCallout and FatSetPurgeFailureMode still used MSVC try/finally, and the IsAWrite locals were guarded out of the Win8 code paths the NT10 target now compiles.

--- drivers/filesystems/fastfat/create.c
+++ drivers/filesystems/fastfat/create.c
@@ -429,19 +429,19 @@ Return Value:
     //  exceptions here and deal with them.
     //
 
-    try {
+    _SEH2_TRY {
 
         CalloutParameters->IrpStatus = FatCommonCreate( CalloutParameters->Create.IrpContext,
                                                         CalloutParameters->Create.Irp );
 
-    } except (FatExceptionFilter( CalloutParameters->Create.IrpContext, GetExceptionInformation() )) {
+    } _SEH2_EXCEPT (FatExceptionFilter( CalloutParameters->Create.IrpContext, _SEH2_GetExceptionInformation() )) {
 
         //
         //  Return the resulting status.
         //
 
-        CalloutParameters->ExceptionStatus = GetExceptionCode();
-    }
+        CalloutParameters->ExceptionStatus = _SEH2_GetExceptionCode();
+    } _SEH2_END;
 
 }
 
