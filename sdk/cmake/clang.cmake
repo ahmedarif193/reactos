8b2715657f016fe550a08bf487b05196de09ddc1
Ahmed ARIF <arif.ing@outlook.com>
2026-06-13 20:31:40 +0200

    [CMAKE] Drop inert -fasync-exceptions from the ARM64 Clang build

--- sdk/cmake/clang.cmake
+++ sdk/cmake/clang.cmake
@@ -183,7 +183,7 @@ elseif(ARCH STREQUAL "amd64")
     add_compile_options(-Wno-error)
 elseif(ARCH STREQUAL "arm64")
     add_compile_options(-fno-optimize-sibling-calls -fno-omit-frame-pointer -mstrict-align)
-    add_compile_options("$<$<NOT:$<COMPILE_LANGUAGE:ASM>>:-fasync-exceptions;-fno-builtin-memcpy;-fno-builtin-memmove;-fno-builtin-memset>")
+    add_compile_options("$<$<NOT:$<COMPILE_LANGUAGE:ASM>>:-fno-builtin-memcpy;-fno-builtin-memmove;-fno-builtin-memset>")
 endif()
 
 # Other
