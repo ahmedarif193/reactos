207c0183c6e85d0132ee68f9dfe1283dad509c3c
Ahmed ARIF <arif.ing@outlook.com>
2026-06-20 11:26:52 +0200

    [NTOS:KE][ARM64] Reset SubNode IdleSmtSet after SMT topology finalize

--- ntoskrnl/ke/arm64/stubs.c
+++ ntoskrnl/ke/arm64/stubs.c
@@ -797,6 +797,7 @@ KeStartAllProcessors(
         KiNode0SubNode.Affinity = KeActiveProcessors;
         KiNode0SubNode.SiblingMask = KeActiveProcessors;
         KiNode0SubNode.NonParkedSet = KeActiveProcessors;
+        KiNode0SubNode.IdleSmtSet = 0;
         KiNode0SubNode.Lowest = 0;
         if (BitScanReverseAffinity(&HighestCpu, KeActiveProcessors))
             KiNode0SubNode.Highest = HighestCpu;
