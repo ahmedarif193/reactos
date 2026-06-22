d6d17a7701f1b00438c4f9e1dbe57ae0a309f8ea
Ahmed ARIF <arif.ing@outlook.com>
2026-06-22 07:26:59 +0200

    [NTOS][HAL][AMD64] Bring up SMP application processors
    AP startup trampoline + HalStartNextProcessor + KeStartAllProcessors; build hal.dll as MP (fixes 0x79); use KTHREAD.Running as the NT10 swap barrier.

--- hal/halx86/smp/amd64/spinup.c
+++ hal/halx86/smp/amd64/spinup.c
@@ -1,8 +1,9 @@
 /*
- * PROJECT:     ReactOS Kernel
+ * PROJECT:     ReactOS HAL
  * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
  * PURPOSE:     AMD64 Application Processor (AP) spinup setup
  * COPYRIGHT:   Copyright 2023 Justin Miller <justin.miller@reactos.org>
+ *              Copyright 2026 Ahmed ARIF <arif.ing@outlook.com>
  */
 
 /* INCLUDES ******************************************************************/
@@ -13,12 +14,108 @@
 #define NDEBUG
 #include <debug.h>
 
+/* GLOBALS *******************************************************************/
+
+extern BOOLEAN HalpOnlyBootProcessor;
+extern PPROCESSOR_IDENTITY HalpProcessorIdentity;
+extern PHYSICAL_ADDRESS HalpLowStubPhysicalAddress;
+extern PVOID HalpLowStub;
+extern HALP_APIC_INFO_TABLE HalpApicInfoTable;
+
+/* Trampoline symbols (hal/halx86/smp/amd64/apentry.S) */
+extern UCHAR HalpAPEntry16[];
+extern UCHAR HalpAPEntry16End[];
+extern UCHAR HalpAPEntryProt32[];
+extern UCHAR HalpAPEntryLong64[];
+extern UCHAR HalpAPEntryJump32[];
+extern UCHAR HalpAPEntryJump64[];
+extern UCHAR HalpAPEntryProcState[];
+extern UCHAR HalpAPEntryHigh[];
+extern UCHAR HalpAPEntryPmlRoot[];
+extern UCHAR HalpAPEntryGdtr[];
+extern UCHAR HalpAPEntryGdt[];
+extern UCHAR HalpAPEntry64[];
+
+ULONG HalpStartedProcessorCount = 1;
+
+#define PTE_PRESENT_WRITE   0x003
+#define PTE_LARGE_IDENTITY  0x083 /* Present | Write | PageSize, PFN 0 */
+
+#define StubOffset(sym)     ((ULONG_PTR)(sym) - (ULONG_PTR)HalpAPEntry16)
+#define StubVa(sym)         ((PUCHAR)HalpLowStub + StubOffset(sym))
+#define StubPa(sym)         ((ULONG)(HalpLowStubPhysicalAddress.QuadPart + StubOffset(sym)))
+
+/* FUNCTIONS *****************************************************************/
+
+static
+ULONG
+HalpSetupTemporaryMappings(VOID)
+{
+    PULONG64 Pml4 = (PULONG64)((PUCHAR)HalpLowStub + 1 * PAGE_SIZE);
+    PULONG64 Pdpt = (PULONG64)((PUCHAR)HalpLowStub + 2 * PAGE_SIZE);
+    PULONG64 Pd = (PULONG64)((PUCHAR)HalpLowStub + 3 * PAGE_SIZE);
+    PHYSICAL_ADDRESS PhysicalAddress;
+
+    /* Start from a copy of the running address space (kernel higher half) */
+    RtlCopyMemory(Pml4, MiAddressToPxe((PVOID)0), PAGE_SIZE);
+
+    /* Identity-map the low 2 MB so the real-mode/protected-mode stub runs */
+    PhysicalAddress = MmGetPhysicalAddress(Pdpt);
+    Pml4[0] = (PhysicalAddress.QuadPart & ~0xFFFULL) | PTE_PRESENT_WRITE;
+
+    PhysicalAddress = MmGetPhysicalAddress(Pd);
+    Pdpt[0] = (PhysicalAddress.QuadPart & ~0xFFFULL) | PTE_PRESENT_WRITE;
+
+    Pd[0] = PTE_LARGE_IDENTITY;
+
+    PhysicalAddress = MmGetPhysicalAddress(Pml4);
+    ASSERT(PhysicalAddress.QuadPart < 0x100000000);
+    return (ULONG)PhysicalAddress.QuadPart;
+}
+
 BOOLEAN
 NTAPI
 HalStartNextProcessor(
     _In_ PLOADER_PARAMETER_BLOCK LoaderBlock,
     _In_ PKPROCESSOR_STATE ProcessorState)
 {
-    //TODO:
-    return FALSE;
+    ULONG TemporaryCr3;
+
+    UNREFERENCED_PARAMETER(LoaderBlock);
+
+    /* Bail out if we only use the boot CPU */
+    if (HalpOnlyBootProcessor)
+        return FALSE;
+
+    /* Bail out if we have started all available CPUs */
+    if (HalpStartedProcessorCount == HalpApicInfoTable.ProcessorCount)
+        return FALSE;
+
+    /* Build the temporary page tables used during the mode transition */
+    TemporaryCr3 = HalpSetupTemporaryMappings();
+    if (!TemporaryCr3)
+        return FALSE;
+
+    /* Copy the trampoline into low memory */
+    RtlCopyMemory(HalpLowStub, HalpAPEntry16, StubOffset(HalpAPEntry16End));
+
+    /* Patch the far-jump targets (selectors are already baked in) */
+    *(PULONG)StubVa(HalpAPEntryJump32) = StubPa(HalpAPEntryProt32);
+    *(PULONG)StubVa(HalpAPEntryJump64) = StubPa(HalpAPEntryLong64);
+
+    /* Patch the data consumed by the trampoline */
+    *(PULONG64)StubVa(HalpAPEntryProcState) = (ULONG64)(ULONG_PTR)ProcessorState;
+    *(PULONG64)StubVa(HalpAPEntryHigh) = (ULONG64)(ULONG_PTR)HalpAPEntry64;
+    *(PULONG)StubVa(HalpAPEntryPmlRoot) = TemporaryCr3;
+
+    /* Trampoline GDTR: 4 entries, base points at the copied GDT */
+    *(PUSHORT)StubVa(HalpAPEntryGdtr) = (4 * sizeof(ULONG64)) - 1;
+    *(PULONG)(StubVa(HalpAPEntryGdtr) + 2) = StubPa(HalpAPEntryGdt);
+
+    /* Fire INIT-SIPI-SIPI */
+    ApicStartApplicationProcessor(HalpStartedProcessorCount, HalpLowStubPhysicalAddress);
+
+    HalpStartedProcessorCount++;
+
+    return TRUE;
 }
--- ntoskrnl/ke/amd64/mproc.c
+++ ntoskrnl/ke/amd64/mproc.c
@@ -3,6 +3,7 @@
  * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
  * PURPOSE:     Architecture specific source file to hold multiprocessor functions
  * COPYRIGHT:   Copyright 2023 Justin Miller <justin.miller@reactos.org>
+ *              Copyright 2026 Ahmed ARIF <arif.ing@outlook.com>
  */
 
 /* INCLUDES *****************************************************************/
@@ -12,6 +13,33 @@
 #define NDEBUG
 #include <debug.h>
 
+/* GLOBALS ****************************************************************/
+
+CODE_SEG("INIT")
+VOID
+KiInitializeProcessorBootStructures(
+    _In_ ULONG ProcessorNumber,
+    _Out_ PKIPCR Pcr,
+    _In_ PKGDTENTRY64 GdtBase,
+    _In_ PKIDTENTRY64 IdtBase,
+    _In_ PKTSS64 TssBase,
+    _In_ PKTHREAD IdleThread,
+    _In_ PVOID KernelStack,
+    _In_ PVOID DpcStack,
+    _In_ PVOID DoubleFaultStack,
+    _In_ PVOID NmiStack);
+
+typedef struct _APINFO
+{
+    DECLSPEC_ALIGN(PAGE_SIZE) KIDTENTRY64 Idt[256];
+    DECLSPEC_ALIGN(PAGE_SIZE) KGDTENTRY64 Gdt[128];
+    DECLSPEC_ALIGN(16) UINT8 DoubleFaultStack[DOUBLE_FAULT_STACK_SIZE];
+    DECLSPEC_ALIGN(16) UINT8 NmiStack[DOUBLE_FAULT_STACK_SIZE];
+    KIPCR Pcr;
+    ETHREAD Thread;
+    KTSS64 Tss;
+} APINFO, *PAPINFO;
+
 /* FUNCTIONS *****************************************************************/
 
 CODE_SEG("INIT")
@@ -19,5 +47,109 @@ VOID
 NTAPI
 KeStartAllProcessors(VOID)
 {
-    UNIMPLEMENTED;
+    PVOID KernelStack = NULL;
+    PVOID DPCStack = NULL;
+    PAPINFO APInfo = NULL;
+    PKPROCESSOR_STATE ProcessorState;
+    KDESCRIPTOR Gdtr, Idtr;
+    ULONG ProcessorCount;
+    ULONG MaximumProcessors;
+
+    MaximumProcessors = KeMaximumProcessors;
+    if (KeNumprocSpecified)
+        MaximumProcessors = min(MaximumProcessors, KeNumprocSpecified);
+    if (KeBootprocSpecified)
+        MaximumProcessors = min(MaximumProcessors, KeBootprocSpecified);
+
+    /* Start at 1, the boot processor is already running */
+    for (ProcessorCount = 1; ProcessorCount < MaximumProcessors; ++ProcessorCount)
+    {
+        KernelStack = NULL;
+        DPCStack = NULL;
+
+        APInfo = ExAllocatePoolZero(NonPagedPool, sizeof(*APInfo), TAG_KERNEL);
+        if (!APInfo)
+            break;
+        ASSERT(ALIGN_DOWN_POINTER_BY(APInfo, PAGE_SIZE) == APInfo);
+
+        KernelStack = MmCreateKernelStack(FALSE, 0);
+        if (!KernelStack)
+            break;
+
+        DPCStack = MmCreateKernelStack(FALSE, 0);
+        if (!DPCStack)
+            break;
+
+        /* Clone the boot processor's descriptor tables */
+        __sgdt(&Gdtr.Limit);
+        __sidt(&Idtr.Limit);
+        RtlCopyMemory(APInfo->Gdt, Gdtr.Base, Gdtr.Limit + 1);
+        RtlCopyMemory(APInfo->Idt, Idtr.Base, Idtr.Limit + 1);
+
+        /* Initialize the PCR, TSS and the GDT TSS descriptor for this CPU */
+        KiInitializeProcessorBootStructures(ProcessorCount,
+                                            &APInfo->Pcr,
+                                            APInfo->Gdt,
+                                            APInfo->Idt,
+                                            &APInfo->Tss,
+                                            (PKTHREAD)&APInfo->Thread,
+                                            KernelStack,
+                                            DPCStack,
+                                            &APInfo->DoubleFaultStack[DOUBLE_FAULT_STACK_SIZE],
+                                            &APInfo->NmiStack[DOUBLE_FAULT_STACK_SIZE]);
+
+        /* Build the processor state the trampoline restores in long mode */
+        ProcessorState = &APInfo->Pcr.Prcb.ProcessorState;
+        RtlZeroMemory(ProcessorState, sizeof(*ProcessorState));
+
+        ProcessorState->SpecialRegisters.Cr0 = __readcr0();
+        ProcessorState->SpecialRegisters.Cr3 = __readcr3();
+        ProcessorState->SpecialRegisters.Cr4 = __readcr4();
+
+        ProcessorState->ContextFrame.SegCs = KGDT64_R0_CODE;
+        ProcessorState->ContextFrame.SegDs = KGDT64_R0_DATA;
+        ProcessorState->ContextFrame.SegEs = KGDT64_R0_DATA;
+        ProcessorState->ContextFrame.SegSs = KGDT64_R0_DATA;
+        ProcessorState->ContextFrame.SegFs = KGDT64_R0_DATA;
+        ProcessorState->ContextFrame.SegGs = KGDT64_R0_DATA;
+
+        ProcessorState->SpecialRegisters.Gdtr.Base = (PVOID)APInfo->Gdt;
+        ProcessorState->SpecialRegisters.Gdtr.Limit = sizeof(APInfo->Gdt) - 1;
+        ProcessorState->SpecialRegisters.Idtr.Base = (PVOID)APInfo->Idt;
+        ProcessorState->SpecialRegisters.Idtr.Limit = sizeof(APInfo->Idt) - 1;
+        ProcessorState->SpecialRegisters.Tr = KGDT64_SYS_TSS;
+
+        ProcessorState->ContextFrame.Rsp = ALIGN_DOWN_BY((ULONG64)KernelStack, 16) - 8;
+        ProcessorState->ContextFrame.Rip = (ULONG64)(ULONG_PTR)KiSystemStartup;
+        ProcessorState->ContextFrame.Rcx = (ULONG64)(ULONG_PTR)KeLoaderBlock;
+        ProcessorState->ContextFrame.EFlags = __readeflags() & ~EFLAGS_INTERRUPT_MASK;
+
+        /* Hand the new processor's data to the loader block */
+        KeLoaderBlock->KernelStack = (ULONG_PTR)KernelStack;
+        KeLoaderBlock->Prcb = (ULONG_PTR)&APInfo->Pcr.Prcb;
+        KeLoaderBlock->Thread = (ULONG_PTR)&APInfo->Thread;
+
+        DPRINT1("KeStartAllProcessors: starting CPU %lu\n", ProcessorCount);
+        if (!HalStartNextProcessor(KeLoaderBlock, ProcessorState))
+            break;
+
+        /* Wait for the AP to consume the loader block (KiInitializeKernel) */
+        while (KeLoaderBlock->Prcb != 0)
+        {
+            KeMemoryBarrier();
+            YieldProcessor();
+        }
+    }
+
+    /* The last allocation belongs to a CPU that did not start */
+    ProcessorCount--;
+
+    if (APInfo)
+        ExFreePoolWithTag(APInfo, TAG_KERNEL);
+    if (KernelStack)
+        MmDeleteKernelStack(KernelStack, FALSE);
+    if (DPCStack)
+        MmDeleteKernelStack(DPCStack, FALSE);
+
+    DPRINT1("KeStartAllProcessors: started %lu processor(s)\n", ProcessorCount);
 }
