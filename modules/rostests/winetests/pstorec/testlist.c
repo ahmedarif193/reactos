b1ec9f4cc37a8b59e606ab7fccc60c734bc7f143
Ahmed ARIF <arif.ing@outlook.com>
2026-08-02 22:42:23 +0100

    [PSTOREC] Sync with Wine 11.14

---
+++ modules/rostests/winetests/pstorec/testlist.c
@@ -0,0 +1,12 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_pstorec(void);
+
+const struct test winetest_testlist[] =
+{
+    { "pstorec", func_pstorec },
+    { 0, 0 }
+};
