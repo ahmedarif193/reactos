366cbf668a49a59d974c91587edaac88bed5dcf7
Ahmed ARIF <arif.ing@outlook.com>
2026-08-02 20:22:01 +0100

    [URL] Sync with Wine 11.14

--- dll/win32/url/url_main.c
+++ dll/win32/url/url_main.c
@@ -32,24 +32,6 @@
 WINE_DEFAULT_DEBUG_CHANNEL(url);
 
 /***********************************************************************
- *		DllMain  (URL.@)
- */
-BOOL WINAPI DllMain( HINSTANCE inst, DWORD reason, LPVOID reserved )
-{
-    switch(reason)
-    {
-#ifndef __REACTOS__
-    case DLL_WINE_PREATTACH:
-        return FALSE;  /* prefer native version */
-#endif
-    case DLL_PROCESS_ATTACH:
-        DisableThreadLibraryCalls( inst );
-        break;
-    }
-    return TRUE;
-}
-
-/***********************************************************************
  * AddMIMEFileTypesPS (URL.@)
  *
  * Build and Manage a Filetype-Association Property Sheet
@@ -74,7 +56,7 @@ DWORD WINAPI AddMIMEFileTypesPS(VOID * unknown1, LPPROPSHEETHEADERA lppsh)
  */
 BOOL WINAPI InetIsOffline(DWORD flags)
 {
-    FIXME("(%08x): stub!\n", flags);
+    FIXME("(%08lx): stub!\n", flags);
 
     return FALSE;
 }
@@ -107,12 +89,7 @@ void WINAPI FileProtocolHandlerA(HWND hWnd, HINSTANCE hInst, LPCSTR pszUrl, int 
  */
 void WINAPI OpenURLA(HWND hwnd, HINSTANCE inst, LPCSTR cmdline, INT show)
 {
-#ifdef __REACTOS__
-    TRACE("(%p, %p, %s, %d)\n", hwnd, inst, debugstr_a(cmdline), show);
-    ShellExecuteA(hwnd, NULL, cmdline, NULL, NULL, show);
-#else
     FIXME("(%p, %p, %s, %d): stub!\n", hwnd, inst, debugstr_a(cmdline), show);
-#endif
 }
 
 /***********************************************************************
