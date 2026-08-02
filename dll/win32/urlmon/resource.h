ab4dd7a8fdd8f9031f122214138b612a587275f2
Ahmed ARIF <arif.ing@outlook.com>
2026-08-02 20:47:33 +0100

    [URLMON] Sync with Wine 11.14

--- dll/win32/urlmon/resource.h
+++ dll/win32/urlmon/resource.h
@@ -16,8 +16,6 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#pragma once
-
 #define ID_AXINSTALL_WARNING_DLG    1000
 #define ID_AXINSTALL_LOCATION       1001
 #define ID_AXINSTALL_INSTALL_BTN    1002
@@ -26,3 +24,14 @@
 #define IDS_AXINSTALL_FAILURE       1100
 #define IDS_AXINSTALL_INSTALLN      1101
 #define IDS_AXINSTALL_INSTALL       1102
+
+#define IDS_SEC_ZONE0_NAME          2000
+#define IDS_SEC_ZONE1_NAME          2001
+#define IDS_SEC_ZONE2_NAME          2002
+#define IDS_SEC_ZONE3_NAME          2003
+#define IDS_SEC_ZONE4_NAME          2004
+#define IDS_SEC_ZONE0_DESC          2100
+#define IDS_SEC_ZONE1_DESC          2101
+#define IDS_SEC_ZONE2_DESC          2102
+#define IDS_SEC_ZONE3_DESC          2103
+#define IDS_SEC_ZONE4_DESC          2104
