fc5dcc17fc205f6be5ff498f9eac02bb80ceac24
Ahmed ARIF <arif.ing@outlook.com>
2026-08-02 22:31:56 +0100

    [MGMTAPI] Sync with Wine 11.14

--- sdk/include/psdk/mgmtapi.h
+++ sdk/include/psdk/mgmtapi.h
@@ -1,55 +1,24 @@
 /*
-  mgmtapi.h - Header file for the SNMP Management API
+ * Copyright 2024 Alex Henrie
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
 
-  Written by Filip Navara <xnavara@volny.cz>
+#ifndef _INC_MGMTAPI
+#define _INC_MGMTAPI
 
-  References (2003-08-25):
-    https://web.archive.org/web/20050113043001/http://msdn.microsoft.com/library/en-us/snmp/snmp/snmp_reference.asp
+typedef void *LPSNMP_MGR_SESSION;
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-*/
-
-#ifndef _MGMTAPI_H
-#define _MGMTAPI_H
-
-#ifndef _SNMP_H
-#include <snmp.h>
-#endif
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#define SNMP_MGMTAPI_TIMEOUT	40
-#define SNMP_MGMTAPI_SELECT_FDERRORS	41
-#define SNMP_MGMTAPI_TRAP_ERRORS	42
-#define SNMP_MGMTAPI_TRAP_DUPINIT	43
-#define SNMP_MGMTAPI_NOTRAPS	44
-#define SNMP_MGMTAPI_AGAIN	45
-#define SNMP_MGMTAPI_INVALID_CTL	46
-#define SNMP_MGMTAPI_INVALID_SESSION	47
-#define SNMP_MGMTAPI_INVALID_BUFFER	48
-#define MGMCTL_SETAGENTPORT	1
-
-#ifndef RC_INVOKED
-
-typedef PVOID LPSNMP_MGR_SESSION;
-
-BOOL WINSNMPAPI SnmpMgrClose(LPSNMP_MGR_SESSION);
-BOOL WINSNMPAPI SnmpMgrCtl(LPSNMP_MGR_SESSION,DWORD,LPVOID,DWORD,LPVOID,DWORD,LPDWORD);
-BOOL WINSNMPAPI SnmpMgrGetTrap(AsnObjectIdentifier*,AsnNetworkAddress*,AsnInteger*,AsnInteger*,AsnTimeticks*,SnmpVarBindList*);
-BOOL WINSNMPAPI SnmpMgrGetTrapEx(AsnObjectIdentifier*,AsnNetworkAddress*,AsnNetworkAddress*,AsnInteger*,AsnInteger*,AsnOctetString*,AsnTimeticks*,SnmpVarBindList*);
-BOOL WINSNMPAPI SnmpMgrOidToStr(AsnObjectIdentifier*,LPSTR*);
-LPSNMP_MGR_SESSION WINSNMPAPI SnmpMgrOpen(LPSTR,LPSTR,INT,INT);
-INT WINSNMPAPI SnmpMgrRequest(LPSNMP_MGR_SESSION,BYTE,SnmpVarBindList*,AsnInteger*,AsnInteger*);
-BOOL WINSNMPAPI SnmpMgrStrToOid(LPSTR,AsnObjectIdentifier*);
-BOOL WINSNMPAPI SnmpMgrTrapListen(HANDLE*);
-
-#endif /* RC_INVOKED */
-
-#ifdef __cplusplus
-}
-#endif
-#endif
+#endif /* _INC_MGMTAPI */
