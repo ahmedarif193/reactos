290cbf9177bb0682e1e1a28ad3ad483af9d5233b
Ahmed ARIF <arif.ing@outlook.com>
2026-08-02 22:40:41 +0100

    [WING32] Sync with Wine 11.14

---
+++ modules/rostests/winetests/wing32/testlist.c
@@ -0,0 +1,12 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_wing32(void);
+
+const struct test winetest_testlist[] =
+{
+    { "wing32", func_wing32 },
+    { 0, 0 }
+};
