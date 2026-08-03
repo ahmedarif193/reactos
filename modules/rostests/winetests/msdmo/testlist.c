5981f7dbad314cd7bbc2517b1e1fb3c87934f856
Ahmed ARIF <arif.ing@outlook.com>
2026-08-03 14:27:26 +0100

    [MSDMO] Sync with Wine 11.14

---
+++ modules/rostests/winetests/msdmo/testlist.c
@@ -0,0 +1,12 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_msdmo(void);
+
+const struct test winetest_testlist[] =
+{
+    { "msdmo", func_msdmo },
+    { 0, 0 }
+};
