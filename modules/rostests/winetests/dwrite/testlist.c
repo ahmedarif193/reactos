daedaf9f457ff40637656db481da9ce1e86b7047
Ahmed ARIF <arif.ing@outlook.com>
2026-08-03 03:50:01 +0100

    [DWRITE] Import Wine 11.14 implementation

---
+++ modules/rostests/winetests/dwrite/testlist.c
@@ -0,0 +1,16 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_analyzer(void);
+extern void func_font(void);
+extern void func_layout(void);
+
+const struct test winetest_testlist[] =
+{
+    { "analyzer", func_analyzer },
+    { "font", func_font },
+    { "layout", func_layout },
+    { 0, 0 }
+};
