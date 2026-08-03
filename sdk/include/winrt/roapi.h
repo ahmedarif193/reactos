ef4a6c623a75cd8bf1ce0dc8af79899ea0029755
Ahmed ARIF <arif.ing@outlook.com>
2026-08-03 06:39:21 +0100

    [COMBASE] Sync with Wine 11.14

--- sdk/include/winrt/roapi.h
+++ sdk/include/winrt/roapi.h
@@ -1,27 +1,29 @@
 /*
- * PROJECT:     ReactOS SDK
- * LICENSE:     MIT (https://spdx.org/licenses/MIT)
- * PURPOSE:     WinRT Runtime Object API
- * COPYRIGHT:   Copyright 2024 Timo Kreuzer (timo.kreuzer@reactos.org)
+ * Copyright (C) 2014 Martin Storsjo
+ * Copyright (C) 2016 Michael Müller
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#pragma once
-#define __ROAPI_H_
+#ifndef __WINE_ROAPI_H
+#define __WINE_ROAPI_H
 
-#include <sal.h>
+#include <windef.h>
 #include <activation.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#ifdef _ROAPI_
-   #define ROAPI
-#else
-   #define ROAPI DECLSPEC_IMPORT
-#endif
-
-typedef enum RO_INIT_TYPE
+typedef enum
 {
     RO_INIT_SINGLETHREADED = 0,
     RO_INIT_MULTITHREADED  = 1,
@@ -29,41 +31,25 @@ typedef enum RO_INIT_TYPE
 
 DECLARE_HANDLE(APARTMENT_SHUTDOWN_REGISTRATION_COOKIE);
 
+#ifdef __cplusplus
+typedef struct {} *RO_REGISTRATION_COOKIE;
+#else
 typedef struct _RO_REGISTRATION_COOKIE *RO_REGISTRATION_COOKIE;
-
-ROAPI
-_Check_return_
-HRESULT
-WINAPI
-RoActivateInstance(
-    _In_ HSTRING classid,
-    _Out_ IInspectable **instance
-);
-
-ROAPI
-_Check_return_
-HRESULT
-WINAPI
-RoGetActivationFactory(
-    _In_ HSTRING classid, 
-    _In_ REFIID iid, 
-    _Out_ void **class_factory
-);
-
-ROAPI
-_Check_return_
-HRESULT
-WINAPI
-RoInitialize(
-    _In_ RO_INIT_TYPE initType);
-
-ROAPI
-void
-WINAPI
-RoUninitialize(void);
-
-typedef HRESULT (WINAPI *PFNGETACTIVATIONFACTORY)(_In_ HSTRING classId, _Out_ IActivationFactory ** factory);
+#endif
+typedef HRESULT (WINAPI *PFNGETACTIVATIONFACTORY)(HSTRING, IActivationFactory **);
 
 #ifdef __cplusplus
-} // extern "C"
+extern "C" {
 #endif
+
+HRESULT WINAPI RoActivateInstance(HSTRING classid, IInspectable **instance);
+HRESULT WINAPI RoGetActivationFactory(HSTRING classid, REFIID iid, void **class_factory);
+HRESULT WINAPI RoInitialize(RO_INIT_TYPE type);
+void WINAPI RoUninitialize(void);
+HRESULT WINAPI RoGetApartmentIdentifier(UINT64 *identifier);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* __WINE_ROAPI_H */
