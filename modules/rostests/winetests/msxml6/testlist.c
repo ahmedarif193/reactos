e8073ad3204364027be0eae4d8f85d7f88315b75
Ahmed ARIF <arif.ing@outlook.com>
2026-08-03 10:34:52 +0100

    [MSXML6] Sync with Wine 11.14

--- modules/rostests/winetests/msxml6/testlist.c
+++ modules/rostests/winetests/msxml6/testlist.c
@@ -4,12 +4,14 @@
 #include <wine/test.h>
 
 extern void func_domdoc(void);
+extern void func_httpreq(void);
 extern void func_saxreader(void);
 extern void func_schema(void);
 
 const struct test winetest_testlist[] =
 {
     { "domdoc", func_domdoc },
+    { "httpreq", func_httpreq },
     { "saxreader", func_saxreader },
     { "schema", func_schema },
     { 0, 0 }
