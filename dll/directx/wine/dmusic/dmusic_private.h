ba67cd6bfb443cdd873f6042a98fc789065b610d
Ahmed ARIF <arif.ing@outlook.com>
2026-08-03 02:55:42 +0100

    [WINED3D] Sync with Wine 11.14

--- dll/directx/wine/dmusic/dmusic_private.h
+++ dll/directx/wine/dmusic/dmusic_private.h
@@ -261,7 +261,9 @@ extern void Patch2MIDILOCALE (DWORD dwPatch, LPMIDILOCALE pLocale) DECLSPEC_HIDD
 /* check whether the given DWORD is even (return 0) or odd (return 1) */
 extern int even_or_odd (DWORD number) DECLSPEC_HIDDEN;
 /* FOURCC to string conversion for debug messages */
+#ifndef __REACTOS__
 extern const char *debugstr_fourcc (DWORD fourcc) DECLSPEC_HIDDEN;
+#endif
 /* returns name of given GUID */
 extern const char *debugstr_dmguid (const GUID *id) DECLSPEC_HIDDEN;
 /* Dump whole DMUS_OBJECTDESC struct */
