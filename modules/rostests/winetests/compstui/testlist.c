454996dc683ec534d3bcee3d80e9335c4372acee
Ahmed ARIF <arif.ing@outlook.com>
2026-08-03 08:22:58 +0100

    [COMPSTUI] Sync with Wine 11.14

---
+++ modules/rostests/winetests/compstui/testlist.c
@@ -0,0 +1,12 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_compstui(void);
+
+const struct test winetest_testlist[] =
+{
+    { "compstui", func_compstui },
+    { 0, 0 }
+};
