383bb856999168fc65d34db0219eb662d8511252
Ahmed ARIF <arif.ing@outlook.com>
2026-08-03 20:20:11 +0100

    [TAPI32] Sync with Wine 11.14

---
+++ modules/rostests/winetests/tapi32/testlist.c
@@ -0,0 +1,12 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_tapi(void);
+
+const struct test winetest_testlist[] =
+{
+    { "tapi", func_tapi },
+    { 0, 0 }
+};
