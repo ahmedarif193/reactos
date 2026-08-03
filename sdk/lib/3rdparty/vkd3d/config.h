352f96ad98df24f20e6267810ff08aac07693a33
Ahmed ARIF <arif.ing@outlook.com>
2026-08-03 02:52:05 +0100

    [VKD3D] Import Wine 11.14 shader library

---
+++ sdk/lib/3rdparty/vkd3d/config.h
@@ -0,0 +1,10 @@
+#define PACKAGE_NAME "vkd3d"
+#define PACKAGE_STRING "vkd3d 2.0"
+#define PACKAGE_VERSION "2.0"
+#define PATH_MAX 1024
+#define SONAME_LIBVULKAN "vulkan-1.dll"
+#ifdef __REACTOS__
+#define HAVE__STRTOD_L 1
+#else
+#define HAVE__STRTOF_L 1
+#endif
