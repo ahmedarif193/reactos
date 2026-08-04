dfa2e6c0289c4e7d60328e165edc4e92f2309c02
Ahmed ARIF <arif.ing@outlook.com>
2026-08-04 08:29:19 +0100

    [ODBC32] Sync with Wine 11.14

---
+++ modules/rostests/winetests/odbc32/testlist.c
@@ -0,0 +1,12 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_odbc32(void);
+
+const struct test winetest_testlist[] =
+{
+    { "odbc32", func_odbc32 },
+    { 0, 0 }
+};
