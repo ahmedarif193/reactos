245eecacbd10e1a5c36211483aa4321b3d79ca43
Ahmed ARIF <arif.ing@outlook.com>
2026-08-04 04:47:22 +0100

    [SCHEDSVC] Sync with Wine 11.14

---
+++ modules/rostests/winetests/schedsvc/testlist.c
@@ -0,0 +1,14 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_atsvcapi(void);
+extern void func_rpcapi(void);
+
+const struct test winetest_testlist[] =
+{
+    { "atsvcapi", func_atsvcapi },
+    { "rpcapi", func_rpcapi },
+    { 0, 0 }
+};
