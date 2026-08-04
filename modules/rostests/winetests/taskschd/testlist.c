d3623519d98629b75764d51da06690ca8323edc6
Ahmed ARIF <arif.ing@outlook.com>
2026-08-04 04:55:21 +0100

    [TASKSCHD] Import from Wine 11.14

---
+++ modules/rostests/winetests/taskschd/testlist.c
@@ -0,0 +1,12 @@
+/* Automatically generated file; DO NOT EDIT!! */
+
+#define STANDALONE
+#include <wine/test.h>
+
+extern void func_scheduler(void);
+
+const struct test winetest_testlist[] =
+{
+    { "scheduler", func_scheduler },
+    { 0, 0 }
+};
